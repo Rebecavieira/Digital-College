bd2d66d2effdde8c45b67469e12831405e7fbfbf
infodevestudent <rebecavieira9876@gmail.com>
2023-03-02 11:43:38 -0300

    adiciona arquivos

--- aula23/ibge/js/eventos.js
+++ aula23/ibge/js/eventos.js
@@ -1,6 +1,9 @@
+ document.querySelector(".bloqueio").classList.add("aparece");
+
  fetch('https://servicodados.ibge.gov.br/api/v1/localidades/regioes')
     .then(res => res.json())
     .then(regioes => {
+        document.querySelector(".bloqueio").classList.remove("aparece");
         function addOption(cada) {
             select_regiao.innerHTML += `<option value="${cada.id}">${cada.nome}</option>`;
          }
@@ -9,9 +12,11 @@
     });
 
 function buscarEstados (){
+    document.querySelector(".bloqueio").classList.add("aparece");
     fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/regioes/${select_regiao.value}/estados`)
         .then(res => res.json())
         .then(estados => {
+            document.querySelector(".bloqueio").classList.remove("aparece");
             select_estado.innerHTML = "<option>-- Selecione --</option>";
             select_cidade.innerHTML = "<option>-- Selecione --</option>";
             function addOption(cada) {
@@ -23,9 +28,11 @@ function buscarEstados (){
 }
 
 function buscarCidades (){
+    document.querySelector(".bloqueio").classList.add("aparece");
     fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${select_estado.value}/municipios`)
         .then(res => res.json())
         .then(cidades => {
+            document.querySelector(".bloqueio").classList.remove("aparece");
             select_cidade.innerHTML = "<option>-- Selecione --</option>";
             function addOption(cada) {
                 select_cidade.innerHTML += `<option value="${cada.id}">${cada.nome}</option>`;
