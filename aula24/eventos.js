14148bc416c4e834d39f280673b38040f571ef0b
infodevestudent <rebecavieira9876@gmail.com>
2023-03-03 09:47:46 -0300

    adiciona arquivos

---
+++ aula24/eventos.js
@@ -0,0 +1,18 @@
+
+fetch('https://pokeapi.co/api/v2/pokemon')
+    .then(res => res.json())
+    .then(lista => {
+        lista.results.forEach(cada => {
+            pokemons.innerHTML += `<option value="${cada.url}">${cada.name}</option>`;
+        });
+        buscarPokemon();
+});
+
+function buscarPokemon(){
+    fetch(pokemons.value)
+        .then(res => res.json())
+        .then(pokemon => {
+            document.querySelector(".pokemon").innerHTML = `<img src="${pokemon.sprites.front_default}">`
+        })
+
+} 
