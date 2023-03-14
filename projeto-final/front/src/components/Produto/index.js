8d8ea5cda37f68c2dcfc8a86411f05d29dd1461f
infodevestudent <rebecavieira9876@gmail.com>
2023-03-14 11:41:15 -0300

    arquivos atualizados

--- projeto-final/front/src/components/Produto/index.js
+++ projeto-final/front/src/components/Produto/index.js
@@ -2,16 +2,16 @@ import { Paper } from "@mui/material";
 import sapato from "./produto.png";
 import { Stack } from "@mui/system";
 
-export default function Produto(){
+export default function Produto(props){
     return(
         <>
             <Stack>
                 <Paper elevation={3}>
-                    <img src={sapato}/>
+                    <img src={props.imagem} style={{width: "100%"}}/>
                 </Paper>
             
-                <span>Categoria</span>
-                <h4>Nome do produto</h4>
+                <span>{props.categoria}</span>
+                <h4>{props.nome}</h4>
             </Stack>
            
         </>
--- projeto-final/front/src/pages/Home/index.js
+++ projeto-final/front/src/pages/Home/index.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import Navbar from "../../components/Navbar";
 import Menu from "../../components/Menu";
 import Carousel from "../../components/Carousel";
@@ -8,6 +9,20 @@ import { Grid } from "@mui/material";
 import { Container } from "@mui/system";
 
 export default function Home(){
+
+        const [listaProdutos, setListaProdutos] = React.useState([]);
+        function buscarProdutos(){
+          fetch("https://63388a25383946bc7fe9a891.mockapi.io/digital-store-users")
+                .then(res => res.json())
+                    .then(lista => {
+                        setListaProdutos(lista)
+                    });
+        }
+        React.useEffect(() => {
+            buscarProdutos();
+    
+        }, [])
+
     return(
         <div>
 
@@ -15,32 +30,18 @@ export default function Home(){
             <Menu />
             <Carousel />
             <ColecoesDestaque />
+            {listaProdutos.length}
             <Container>
-                <Grid container spacing={2}>
-                    <Grid item md={3}>
-                        <Produto />
-                    </Grid>
-                    <Grid item md={3}>
-                        <Produto />
-                    </Grid>
-                    <Grid item md={3}>
-                        <Produto />
-                    </Grid>
-                    <Grid item md={3}>
-                        <Produto />
-                    </Grid>
-                    <Grid item md={3}>
-                        <Produto />
-                    </Grid>
-                    <Grid item md={3}>
-                        <Produto />
-                    </Grid>
-                    <Grid item md={3}>
-                        <Produto />
-                    </Grid>
-                    <Grid item md={3}>
-                        <Produto />
-                    </Grid>
+                <Grid container spacing={3}>
+                        {
+                            listaProdutos.map((cadaProduto) => {
+                                return(
+                                    <Grid item md={3.5}>
+                                        <Produto nome={cadaProduto.nome} categoria={cadaProduto.categoria} imagem={cadaProduto.imgUrl} />
+                                    </Grid>
+                                );
+                            })
+                        }
                 </Grid>
             </Container>
             <Footer />
