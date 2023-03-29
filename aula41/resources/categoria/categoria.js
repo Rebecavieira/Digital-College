f24fdea121e4190e54be148668e70c3968eca01b
infodevestudent <rebecavieira9876@gmail.com>
2023-03-29 12:00:44 -0300

    adiciona

---
+++ aula41/resources/categoria/categoria.js
@@ -0,0 +1,23 @@
+const categorias = require("./model")
+
+function cadastrar(dados){
+    categorias.push(dados);
+    return JSON.stringify(dados);
+}
+ 
+function buscar(){
+    return JSON.stringify(categorias);
+}
+
+function buscarUm(id){
+    let busca = categorias.filter((cada) => {
+        return cada.id == id;
+    })
+    return JSON.stringify(busca);
+}
+
+module.exports = {
+    buscar,
+    cadastrar,
+    buscarUm,
+}; 
---
+++ aula41/resources/cliente/router.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cliente = require('./cliente');
+
+//criando um router para gerenciar os endpoints de cliente
+const app = express.Router();
+
+app.get('/clientes', (req, res) => {
+    res.send(cliente.buscar());
+});
+
+app.post('/clientes', (req, res) => {
+    res.send(cliente.cadastrar());
+});
+
+//exportando todas as rotas criadas aqui
+module.exports = app;
