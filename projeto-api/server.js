0091dcfc91b502d3e6480d4dc3e7108ba5b628ed
infodevestudent <rebecavieira9876@gmail.com>
2023-03-30 11:58:57 -0300

    arquivos

--- projeto-api/server.js
+++ projeto-api/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
-
+const categoryRoutes = require("./resources/category/routes");
 const app = express();
 
-app.listen(3000, () => {
+app.use(categoryRoutes);
+
+app.listen(8000, () => {
     console.log('------------');
     console.log('---PRONTO---');
     console.log('------------');
