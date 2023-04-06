015e0fcf87abe7fa4def1506366ea2973328f513
infodevestudent <rebecavieira9876@gmail.com>
2023-04-06 10:17:18 -0300

    arquivos

--- projeto-api/server.js
+++ projeto-api/server.js
@@ -5,8 +5,13 @@ const produtoRoutes = require("./resources/produto/routes");
 const pedidoRoutes = require("./resources/pedido/routes");
 const usersRoutes = require("./resources/users/routes");
 const cors = require('cors');
+const swagger = require("swagger-ui-express");
+const docs = require('./docs.json')
 
 const app = express();
+
+// criando rota da documentação
+app.use('/documentacao', swagger.serve, swagger.setup(docs));
 
 app.use(cors());
 app.use(express.json());
@@ -15,6 +20,7 @@ app.use(bannerRoutes);
 app.use(produtoRoutes);
 app.use(pedidoRoutes);
 app.use(usersRoutes);
+
 
 app.listen(8000, () => {
     console.log('--------------');
